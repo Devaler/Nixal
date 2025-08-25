e6e81f7eb697a80f9a20c7dc3e4a41dc077b803f
v <>
2025-08-25 20:18:06 +0200

    first modules

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,6 +2,14 @@
     inputs,
     ...
 }: let
+# Copy raf description?
+modulesPath = ../modules;
+
+rolesPath = modulesPath + /roles;
+# Roles
+gaming = rolesPath + /gaming;
+works = rolesPath + /work;
+
 mkHost = {architecture, roles} :
     inputs.nixpkgs.lib.nixosSystem {
         system = architecture;
