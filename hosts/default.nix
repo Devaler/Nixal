e73813d5e565807ffd0edec091f77f596934b24c
v <>
2025-09-06 10:12:19 +0200

    upgraded mkHost function, hopefully. Still need some tidying

--- hosts/default.nix
+++ hosts/default.nix
@@ -9,17 +9,34 @@
   # Probably add an hardwarePath and hardware in mkHost
 
   commonConfig = ./commonConfig.nix;
-  mkHost = {
-    architecture,
-    roles,
-  }:
-    inputs.nixpkgs.lib.nixosSystem {
+#   mkHost = {
+#     architecture,
+#     roles,
+#   }:
+#     inputs.nixpkgs.lib.nixosSystem {
+#       system = architecture;
+#       modules = roles ++ [commonConfig inputs.nvf.nixosModules.default];
+#     };
+# in {
+#   xixi = mkHost {
+#     architecture = "x86_64-linux";
+#     roles = [];
+#   };
+# }
+
+
+  mkHost = { host, architecture, roles }: {
+    ${host} = inputs.nixpkgs.lib.nixosSystem {
       system = architecture;
-      modules = roles ++ [commonConfig ./test.nix];
+      modules = roles ++ [commonConfig ./${host} inputs.nvf.nixosModules.default];
     };
-in {
-  test = mkHost {
+  };
+
+  hosts = [
+    {
+    host = "xixi";
     architecture = "x86_64-linux";
     roles = [];
-  };
-}
+  }
+  ];
+in builtins.foldl' (a: b: a // b) {} (map mkHost hosts)
